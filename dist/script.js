10486ca5719bd044e4503c7da2177541f045bc33
bruno-ryan <nayr.onurb@gmail.com>
2022-02-12 13:10:32 -0300

    add scroll animation

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";console.log("HELLO");var test=function(){console.log("this is a test")};
+"use strict";function reveal(){for(var e=document.querySelectorAll(".reveal"),t=0;t<e.length;t++){var i=window.innerHeight;e[t].getBoundingClientRect().top<i-150?e[t].classList.add("active"):e[t].classList.remove("active")}}window.addEventListener("scroll",reveal);
 //# sourceMappingURL=script.js.map
